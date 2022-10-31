b32dcfeae96772235092eb959d9bbe4701f5a7ab
Junior Alves <helio.alvs@gmail.com>
2022-10-31 17:55:09 -0300

    PULL

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState }  from 'react';
+import { Image, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+import { Background } from '../../components/Background';
+
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { styles } from './styles';
+
+export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+  const navigation = useNavigation();
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps){
+    navigation.navigate('game', { id, title, bannerUrl });
+  }
+
+  useEffect(() => {
+    fetch('http://172.16.81.20:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data))    
+  }, []);
+
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image
+          source={logoImg}
+          style={styles.logo}
+        />
+
+        <Heading
+          title='Encontre seu duo!'
+          subtitle='Selecione o game que deseja jogar...'
+        />
+
+        <FlatList
+          data={games}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <GameCard 
+              data={item}
+              onPress={() => handleOpenGame(item)}
+            />
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
+      </SafeAreaView>
+    </Background>
+  );
+}
